587826218ff3b8d136bd1536cbfc94f93e8e11da
Wang Yao <yaowang274@gmail.com>
2021-01-23 21:32:19 +1300

    change test5.spec.js

--- cypress/integration/examples/test1.spec.js
+++ cypress/integration/examples/test1.spec.js
@@ -1,13 +1,3 @@
-// describe('Test1',function(){
-//     beforeEach(()=>{
-//         cy.visit('https://www.google.com/')
-//     })
-//     it('Google Search',function(){
-//         cy.get('input[name="q"]').type('Cypress')
-//         cy.get('input').contains('Google Search').click()
-//         cy.get('body').should('contain', 'Cypress')
-//     })
-// })
 describe("Test", function () {
   it("add", function () {
     expect(15 + 10).to.equal(25);
---
+++ cypress/integration/examples/test5.spec.js
@@ -0,0 +1,60 @@
+describe("Hooks", () => {
+  before(() => {
+    cy.visit("http://a.testaddressbook.com/sign_in");
+    cy.get("#session_email").clear().type("yaowang274@gmail.com");
+    cy.get("#session_password").clear().type("wangsong");
+    cy.get("input").contains("Sign in").click();
+    cy.get("h1").should("contain", "Welcome to Address Book");
+    cy.fixture("profile.json").as("profile");
+  });
+
+  beforeEach(() => {});
+
+  it("addAddress", function () {
+    cy.get("a").contains("Addresses").click();
+    cy.url().should("include", "addresses");
+    cy.get(".row.justify-content-center").click();
+    cy.url().should("include", "/new");
+    const firstName = this.profile.firstName;
+    const lastName = this.profile.lastName;
+    const address = this.profile.streetAddress;
+    const City = this.profile.City;
+    const zipCode = this.profile.zipCode;
+    cy.get("#address_first_name")
+      .clear()
+      .type(firstName)
+      .should("have.value", firstName);
+    cy.get("#address_last_name")
+      .clear()
+      .type(lastName)
+      .should("have.value", lastName);
+    cy.get("#address_street_address")
+      .clear()
+      .type(address)
+      .should("have.value", address);
+    cy.get("#address_city").clear().type(City).should("have.value", City);
+    cy.get("#address_zip_code")
+      .clear()
+      .type(zipCode)
+      .should("have.value", zipCode);
+    cy.get("input").contains("Create Address").click();
+    cy.get(".alert-notice").should(
+      "contain",
+      "Address was successfully created."
+    );
+  });
+
+  afterEach(() => {
+    cy.get(".nav-item.nav-link").contains("Addresses").click();
+    cy.get("td").last().contains("Destroy").click();
+    cy.get(".alert-notice").should(
+      "contain",
+      "Address was successfully destroyed."
+    );
+  });
+
+  after(() => {
+    cy.get("a").contains("Sign out").click();
+    cy.get("#clearance").should("be.visible");
+  });
+});
