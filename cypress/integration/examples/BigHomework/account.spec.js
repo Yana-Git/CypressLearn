595b4fa343151849d1de34c7f4812cc64fea6453
Wang Yao <yaowang274@gmail.com>
2021-02-06 22:16:04 +1300

    write big homework

--- cypress/integration/examples/BigHomework/account.spec.js
+++ cypress/integration/examples/BigHomework/account.spec.js
@@ -10,7 +10,7 @@ describe("account function", function () {
 
   it.only("order history and details", function () {
     cy.get("span").contains("Order history and details").click();
-    cy.get("tbody>tr").should("have.length", 3);
+    cy.get("tbody>tr").should("have.length", 4);
 
     //////order reference
     cy.get(".first_item > .history_link").click();
@@ -28,21 +28,57 @@ describe("account function", function () {
     cy.get(".first_item > .history_link > .color-myaccount").click();
     cy.get(".dark > strong").should(
       "contain",
-      "Order Reference GGSIWKUJS -- placed on 02/02/2021"
+      "Order Reference FTZCOVFRM -- placed on 02/03/2021"
     );
-    cy.get(".item > .history_link > .color-myaccount").click();
+    cy.get(".item.alternate_item > .history_link > .color-myaccount").click();
     cy.get(".dark > strong").should(
       "contain",
-      "Order Reference MTGMSMFNO -- placed on 02/02/2021"
+      "Order Reference GGSIWKUJS -- placed on 02/02/2021"
     );
 
+    //////message
+    cy.get("#sendOrderMessage > .submit > .button > span").click();
+    cy.get(".alert > p").contains("There is 1 error");
+    cy.get("select").select("6");
+    cy.get(":nth-child(4) > .form-control").clear().type("lala");
+    cy.get("#sendOrderMessage > .submit > .button > span").click();
+    cy.get(".alert-success").contains(" Message successfully sent");
     //////Date
-    cy.get(".footable-sorted").click();
+    cy.get("tbody").find("tr").eq(4).contains("02/02/2021");
+    cy.get("tbody").find("tr").eq(0).contains("02/03/2021");
+    cy.get("#order-list > thead > tr > :nth-child(2)").click();
+    cy.get("tbody").find("tr").eq(0).contains("02/02/2021");
+    cy.get("tbody").find("tr").eq(3).contains("02/03/2021");
 
     //////total price
+    cy.get('#order-list > thead > tr > [data-hide="phone"]').click();
+    cy.get("tbody").find("tr").eq(4).contains("55");
+    cy.get("tbody").find("tr").eq(0).contains("18.51");
+    cy.get('#order-list > thead > tr > [data-hide="phone"]').click();
+    cy.get("tbody").find("tr").eq(0).contains("55");
+    cy.get("tbody").find("tr").eq(3).contains("18.51");
+
     //////status
-    //////invoice
+    cy.get(":nth-child(1) > .history_state").contains("On backorder");
+
+    //////invoice////失败expected undefined to equal 200
+    // cy.get(":nth-child(1) > .history_link > .footable-toggle").click();
+    // cy.intercept(
+    //   "get",
+    //   "http://automationpractice.com/index.php?controller=pdf-invoice&id_order=282854"
+    // ).as("PdfFile");
+    // cy.get(
+    //   ":nth-child(2) > .footable-row-detail-cell > .footable-row-detail-inner > :nth-child(2) > .footable-row-detail-value > .link-button"
+    // ).click();
+    // cy.wait("@PdfFile").should((response) => {
+    //   expect(response.status).equal(200);
+    // });
+
     //////details
-    //////message
+    cy.get(":nth-child(1) > .history_link > .footable-toggle").click();
+    cy.get(
+      ":nth-child(2) > .footable-row-detail-cell > .footable-row-detail-inner > :nth-child(3) > .footable-row-detail-value > .link-button"
+    ).click();
+    cy.url().should("include", "controller=order");
   });
 });
--- cypress/integration/examples/BigHomework/product.spec.js
+++ cypress/integration/examples/BigHomework/product.spec.js
@@ -5,16 +5,19 @@ describe("product function", function () {
 
   it.only("successful search", function () {
     cy.get(
-      "#homefeatured > .first-in-line.first-item-of-tablet-line > .product-container > .left-block > .product-image-container"
-    )
-
-      .find("span")
-      .contains("Quick view")
-      .invoke("show", { force: true })
-      .click();
-    cy.get("span").contains("Quick view").should("be.visible");
-    cy.get(".product-price").contains("$16.51").should("be.visible");
-    cy.get("span").contains("Add to cart").should("be.visible");
-    cy.get("span").contains("More").should("be.visible");
+      "#homefeatured > .first-in-line.first-item-of-tablet-line > .product-container > .left-block > .product-image-container > .quick-view-wrapper-mobile > .quick-view-mobile"
+    ).click();
+    cy.get("#fancybox-frame1612599635210").should("be.visible");
+    // cy.get(
+    //   "#homefeatured > .first-in-line.first-item-of-tablet-line > .product-container > .left-block > .product-image-container"
+    // )
+    //   .find("a")
+    //   .contains("Quick view")
+    //   .invoke("show")
+    //   .click({ force: true });
+    // cy.get("span").contains("Quick view").should("be.visible");
+    // cy.get(".product-price").contains("$16.51").should("be.visible");
+    // cy.get("span").contains("Add to cart").should("be.visible");
+    // cy.get("span").contains("More").should("be.visible");
   });
 });
