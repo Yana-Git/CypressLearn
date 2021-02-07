3b2e09f9b4fc4ba611c44adc460252651ab1d0da
Wang Yao <yaowang274@gmail.com>
2021-02-07 14:14:14 +1300

    some progress

--- cypress/integration/examples/BigHomework/product.spec.js
+++ cypress/integration/examples/BigHomework/product.spec.js
@@ -5,16 +5,26 @@ describe("product function", function () {
 
   it.only("successful search", function () {
     cy.get(
-      "#homefeatured > .first-in-line.first-item-of-tablet-line > .product-container > .left-block > .product-image-container"
-    )
-      .hover()
-      .find("a")
-      .contains("Quick view")
-      .click({ force: true });
-
-    cy.get("span").contains("Quick view").should("be.visible");
-    cy.get(".product-price").contains("$16.51").should("be.visible");
-    cy.get("span").contains("Add to cart").should("be.visible");
-    cy.get("span").contains("More").should("be.visible");
+      "#homefeatured > .first-in-line.first-item-of-tablet-line > .product-container > .left-block > .product-image-container > .quick-view-wrapper-mobile > .quick-view-mobile"
+    ).click();
+    // .trigger("mouseover")
+    // .find("a")
+    // .contains("Quick view")
+    // .click({ force: true });
+    //cy.get("#bigpic").should("be.visible");
+    cy.get(".fancybox-item").click();
+    // cy.get("span").contains("Quick view").should("be.visible");
+    // cy.get(".product-price").contains("$16.51").should("be.visible");
+    cy.get("span").contains("Add to cart").should("be.visible").click();
+    cy.get(".layer_cart_product > h2").should(
+      "contain",
+      "Product successfully added to your shopping cart"
+    );
+    cy.get(".continue > span").click();
+    cy.url().should("include", "http://automationpractice.com/index.php");
+    cy.get(
+      "#homefeatured > .first-in-line.first-item-of-tablet-line > .product-container > .right-block > .button-container > .lnk_view > span"
+    ).click();
+    cy.url().should("include", "controller=product");
   });
 });
