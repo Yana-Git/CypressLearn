09bcf0eb01e98c28b063cd18dd934ddb521056da
Wang Yao <yaowang274@gmail.com>
2021-02-10 16:51:08 +1300

    finish big homework

--- cypress/integration/examples/BigHomework/product.spec.js
+++ cypress/integration/examples/BigHomework/product.spec.js
@@ -1,77 +1,96 @@
-//<reference types="cypress-iframe" />
-
 describe("product function", function () {
   beforeEach("go to the website", function () {
     cy.visit("http://automationpractice.com/index.php");
-    // cy.get(".login").click();
-    // cy.get("#email").clear().type("yaowang274@gmail.com");
-    // cy.get("#passwd").clear().type("wangsong");
-    // cy.get("#SubmitLogin").click();
-    // cy.url().should("include", "controller=my-account");
-    // cy.get(".icon-home").click();
+    cy.get(".login").click();
+    cy.get("#email").clear().type("yaowang274@gmail.com");
+    cy.get("#passwd").clear().type("wangsong");
+    cy.get("#SubmitLogin").click();
+    cy.url().should("include", "controller=my-account");
+    cy.get(".icon-home").click();
   });
-  const frame = document.getElementById('your-frame-id');
-  frame.contentWindow.postMessage(/*any variable or object here*/, 'http://your-second-site.com');
+  const getIframeDocument = () => {
+    return (
+      cy
+        .get(".fancybox-inner")
+        .find("iframe")
+        // Cypress yields jQuery element, which has the real
+        // DOM element under property "0".
+        // From the real DOM iframe element we can get
+        // the "document" element, it is stored in "contentDocument" property
+        // Cypress "its" command can access deep properties using dot notation
+        // https://on.cypress.io/its
+        .its("0.contentDocument")
+        .should("exist")
+    );
+  };
+  const getIframeBody = () => {
+    // get the document
+    return (
+      getIframeDocument()
+        // automatically retries until body is loaded
+        .its("body")
+        .should("not.be.undefined")
+        // wraps "body" DOM element to allow
+        // chaining more Cypress commands, like ".find(...)"
+        .then(cy.wrap)
+    );
+  };
   it("mouseover", function () {
+    ///add to cart
     cy.get(
       "#homefeatured > .first-in-line.first-item-of-tablet-line > .product-container > .left-block > .product-image-container > .product_img_link > .replace-2x"
     ).rightclick();
-    cy.iframe().find("span").contains("Add to cart").click();
-
-    ///add to cart
-
-    // getIframeBody()
-    //   .find("span")
-    //   .contains("Add to cart")
-    //   .should("be.visible")
-    //   .click();
+    cy.get("span").contains("Add to cart").click();
     cy.get("h2")
       .contains("Product successfully added to your shopping cart")
       .should("be.visible");
     cy.get(".cross").click();
     ///more
-    // cy.get(
-    //   "#homefeatured > .first-in-line.first-item-of-tablet-line > .product-container > .left-block > .product-image-container > .product_img_link > .replace-2x"
-    // ).rightclick();
-    // getIframeBody().find("span").contains("More").should("be.visible").click();
-    // cy.url().should("include", "id_product=1&controller=product");
-    // cy.get(".icon-home").click();
+    cy.get(
+      "#homefeatured > .first-in-line.first-item-of-tablet-line > .product-container > .left-block > .product-image-container > .product_img_link > .replace-2x"
+    ).rightclick();
+    cy.get("span").contains("More").should("be.visible").click();
+    cy.url().should("include", "id_product=1&controller=product");
+    cy.get(".icon-home").click();
     ///quick view pic
-    // cy.get(
-    //   "#homefeatured > .first-in-line.first-item-of-tablet-line > .product-container > .left-block > .product-image-container > .product_img_link > .replace-2x"
-    // ).rightclick();
-    // getIframeBody().find("span").contains("Quick view").click();
-    // getIframeBody().find("#bigpic").should("be.visible");
-    // getIframeBody().find("#thumb_4").trigger("mouseover");
-    // getIframeBody()
-    //   .find("#bigpic")
-    //   .find('[src="http://automationpractice.com/img/p/4/4-cart_default.jpg"]')
-    //   .should("exist");
+    cy.get(
+      "#homefeatured > .first-in-line.first-item-of-tablet-line > .product-container > .left-block > .product-image-container > .product_img_link > .replace-2x"
+    ).rightclick();
+    cy.get("span").contains("Quick view").click();
+    cy.wait(6000);
+    getIframeBody().find("#bigpic").should("be.visible");
+    getIframeBody().find("#thumb_4").trigger("mouseover");
+    getIframeBody()
+      .find(
+        'img[src="http://automationpractice.com/img/p/4/4-large_default.jpg"]'
+      )
+      .should("exist");
     ///share
-    // cy.visit(
-    //   "https://twitter.com/intent/tweet?text=Faded%20Short%20Sleeve%20T-shirts%20http://automationpractice.com/index.php?id_product=1&controller=product",
-    //   {
-    //     onBeforeLoad(win) {
-    //       cy.stub(win, "open");
-    //     },
-    //   }
-    // );
-    // getIframeBody().find("button").contains("Tweet").click();
-    // cy.window().its("open").should("be.called");
+    // cy.intercept(
+    //   "GET",
+    //   "https://accounts.google.com/signin/v2/identifier?passive=1209600&osid=1&continue=https%3A%2F%2Fplus.google.com%2Fup%2F%3Fcontinue%3Dhttps%3A%2F%2Fplus.google.com%2Fshare%3Furl%253Dhttp%3A%2F%2Fautomationpractice.com%2Findex.php%3Fid_product%25253D1%252526controller%25253Dproduct%252526content_only%25253D1&followup=https%3A%2F%2Fplus.google.com%2Fup%2F%3Fcontinue%3Dhttps%3A%2F%2Fplus.google.com%2Fshare%3Furl%253Dhttp%3A%2F%2Fautomationpractice.com%2Findex.php%3Fid_product%25253D1%252526controller%25253Dproduct%252526content_only%25253D1&flowName=GlifWebSignIn&flowEntry=ServiceLogin"
+    // ).as("share");
+    getIframeBody().find("button").contains("Google+").click();
+    // cy.wait("@share").should((request) => {
+    //   expect(request.status).equal(200);
+    // });
 
     ///wishlist
-    // getIframeBody().find("p").contains("Add to wishlist").click();
-    // cy.get("p").contains("Added to your wishlist.").should("exist");
-    // cy.get("a").find('[title="Close"]').click();
+    getIframeBody().find("p").contains("Add to wishlist").click();
+    getIframeBody()
+      .find(".fancybox-error")
+      .should("contain", "Added to your wishlist.");
+
+    getIframeBody().find('a[title="Close"]').click();
     ///add to cart
-    // getIframeBody().find(".icon-plus").click();
-    // getIframeBody().find("select").select("3");
-    // getIframeBody().find("a").find('[title="Blue"]').click();
-    // getIframeBody().find("span").contains("Add to cart").click();
-    // cy.get("h2")
-    //   .contains("Product successfully added to your shopping cart")
-    //   .should("be.visible");
-    // cy.get("span").contains("Blue, L").should("exist");
-    // cy.get("#layer_cart_product_quantity").should("contain", "2");
+    getIframeBody().find(".icon-plus").click();
+    getIframeBody().find("select").select("3");
+    getIframeBody().find('a[title="Blue"]').click();
+    getIframeBody().find("span").contains("Add to cart").click();
+    cy.get("h2")
+      .contains("Product successfully added to your shopping cart")
+      .should("be.visible");
+    cy.get("span").contains("Blue, L").should("exist");
+    cy.get("#layer_cart_product_quantity").should("contain", "2");
   });
 });
